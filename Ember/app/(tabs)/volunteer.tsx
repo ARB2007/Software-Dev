1a0ced0c1bd53a7788263db952a6b9000320c2bf
ARB2007 <aidanroebriesewitz@gmail.com>
2025-11-22 23:30:01 +0000

    keeping from expiring

--- Ember/app/(tabs)/volunteer.tsx
+++ Ember/app/(tabs)/volunteer.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
-const containerStyle = {
+const containerStyle: React.CSSProperties = {
   width: "100vw",
   height: "100vh",
 };
 
-const center = { lat: 37.841157, lng: -122.551679 };
+const center: google.maps.LatLngLiteral = { lat: 37.841157, lng: -122.551679 };
 
-export default function Volunteer() {
+const Volunteer: React.FC = () => {
+  const apiKey = "AIzaSyBpzfgckoo0yerlIRIaR5RxJGmiC69bstE"; 
+
   return (
-    <LoadScript googleMapsApiKey="AIzaSyBpzfgckoo0yerlIRIaR5RxJGmiC69bstE">
+    <LoadScript googleMapsApiKey={apiKey}>
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
         <Marker position={center} />
       </GoogleMap>
     </LoadScript>
   );
-}
+};
+
+export default Volunteer;
